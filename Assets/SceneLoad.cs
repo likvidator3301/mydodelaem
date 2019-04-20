9b44e68019fa94461c6c432ac96c19dd5202dcd7
likvdiator3301 <isakovanatolii2809@gmail.com>
2019-04-20 10:53:06 +0500

    Добавлено главное меню

---
+++ Assets/SceneLoad.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoad : MonoBehaviour
+{
+    public string Name;
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(Name);
+    }
+}
